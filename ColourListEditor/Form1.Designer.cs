6b43735c675b8e5e4acd817eb54cc0cf400b4f01
PTwr <PTwr@outlook.com>
2019-01-05 14:50:39 +0100

    embeed libs in exe

--- ColourListEditor/Form1.Designer.cs
+++ ColourListEditor/Form1.Designer.cs
@@ -224,7 +224,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Colour list editor";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
